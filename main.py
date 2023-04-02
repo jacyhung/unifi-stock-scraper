b7e05bda547ba101a42ae3d77d25109ebd93ed3e
Jacy Hung <13006956+jacyhung@users.noreply.github.com>
2023-04-01 19:24:52 -0700

    first v

---
+++ main.py
@@ -0,0 +1,30 @@
+from discord_webhook import DiscordWebhook
+from bs4 import BeautifulSoup
+import time
+import requests
+
+TARGET_URL = "https://store.ui.com/collections/unifi-protect/products/unifi-protect-g3-instant-camera"
+WEBHOOK_URL = (
+    "<Discord Webhook URL>"
+)
+
+def notify():
+    webhook = DiscordWebhook(url=WEBHOOK_URL, content='@everyone Product is in stock!')
+    return webhook.execute()
+    print("Notified!")
+def scrape():
+    page = requests.get(TARGET_URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    time.sleep(5)
+    for div in soup.find(id="productDescriptionAndWizard").find_all("div"):
+        if any("in stock" in span.text.lower() for span in div.find_all("span")):
+            print("Found availability.")
+            notify()
+            break
+    else:
+        print("Out of stock.")
+    scrape()
+
+
+if __name__ == "__main__":
+    scrape()
